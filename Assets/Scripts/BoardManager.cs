8c6aea32d659190d95b916121ff3618f95b26aba
Rahmat_Alrafi_Trikada <63140592+RahmatAT11@users.noreply.github.com>
2021-05-12 10:58:58 +0700

    2_Board_as_a_Singleton

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static BoardManager _instance = null;
+
+    public static BoardManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<BoardManager>();
+
+                if (_instance == null)
+                {
+                    Debug.LogError("Fatal Error: BoardManager not Found");
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
